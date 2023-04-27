95631bbe5c7458432e21945549d460ed679eea54
aliza13 <alizaajc13@gmail.com>
2023-04-26 22:06:12 -0700

    Update assign3.cpp

--- W3 Assign Link Queue/assign3.cpp
+++ W3 Assign Link Queue/assign3.cpp
@@ -1,1 +1,29 @@
 #include <iostream>
+
+using std::cout;
+using std::endl;
+
+struct node {
+    int data;
+    node *next;
+};
+
+class LinkedQueue {
+    private:
+        node *top;
+
+    public:
+        Stack(){
+            top = nullptr;
+        }
+    void push(int = newData) {
+        
+        top = newNode
+    }
+}
+
+// has to have a main func no matter what
+int main(){
+
+
+}
